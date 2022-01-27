a48a010084dca6928fdada364226c9ccfbd0a35b
ggenilson <genilsonaraujo20002002@gmail.com>
2022-01-27 10:03:47 +0100

    feat: refactoring categories list

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,19 +2,16 @@ import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoryController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new CategoriesRepository();
-
 categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController.handle(request, response);
+  createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
-});
+categoriesRoutes.get("/", (request, response) =>
+  listCategoryController.handle(request, response)
+);
 
 export { categoriesRoutes };
