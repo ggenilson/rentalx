1fd2bcae817741ace518b6c1dd51faaa102460ae
ggenilson <genilsonaraujo20002002@gmail.com>
2022-01-27 06:37:55 +0100

    chore: config debugger

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.send({ name });
+});
+
 app.listen(3333, () => console.log("Server is running"));
